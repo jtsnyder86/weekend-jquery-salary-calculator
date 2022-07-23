7e68f69f9345681b3ba7b507b514a7446a66b6cc
Josh Snyder <joshua.t.snyder@gmail.com>
2022-07-23 15:04:43 -0500

    added delete button to remove entry (entry only; salary remains in monthly total

--- client.js
+++ client.js
@@ -4,15 +4,17 @@ $(document).ready(calculateNow);
 
 function calculateNow() {  // this is our base function to call jquery
     console.log('calculateNow is working'); // this is checking for functionality -- works!
-
+    // adds the employee info to table in dom
     $('#submitBtn').on('click', addEE)
+    //deletes information added (currently does NOT remove salary from monthly total)
+    $('#container').on('click', '.dleteBtn', removeEntry)
 
 
 
 }
 
 function addEE() {
-    console.log('clicked!');
+    // console.log('clicked!');
     //getter!
     let first = $('#firstInput').val();
     let last = $('#lastInput').val();
@@ -20,8 +22,10 @@ function addEE() {
     let title = $('#titleInput').val();
     let annualSalary = Number($('#salaryInput').val());
 
-    console.log(first,last, id, title, annualSalary);
 
+
+    // console.log(first,last, id, title, annualSalary);
+    //appending to add the input data into the table
     $('#container').append(`
         <tr>
             <td>${first}</td>
@@ -29,7 +33,21 @@ function addEE() {
             <td>${id}</td>
             <td>${title}</td>
             <td>$${annualSalary}</td>
+            <td><button class="dleteBtn">Delete<td>
         <tr/>
             `);
+            // empty the input
+            $('#firstInput').val('');
+            $('#lastInput').val('');
+            $('#idInput').val('');
+            $('#titleInput').val('');
+            $('#salaryInput').val('')
 
+}
+
+function removeEntry() {
+    console.log('time to remove!');
+    // removes last entry
+    $(this).closest('tr').remove();
+    
 }
